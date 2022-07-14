4134f6ab177e2766e43652a0b7259005f97c2f9a
Luuk van der Duim <luukvanderduim@gmail.com>
2022-07-14 23:44:47 +0200

    Manually merge self-contained branch with main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use argh::FromArgs;
-use hashbrown::HashSet;
-use rayon::prelude::*;
-use regex::Regex;
-// use std::collections::HashSet;
-use std::error::Error;
-use std::process::Command;
-use std::sync::{Arc, Mutex};
-use std::{path::PathBuf, str::FromStr};
+use elf_rs::{self, Elf, ElfClass, ElfFile};
+use iced_x86::{CpuidFeature, Decoder, DecoderOptions};
+use std::{
+    collections::HashSet,
+    error::Error,
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 #[derive(FromArgs)]
 /// identify the ABI extentions in use in a binary.
@@ -22,69 +24,49 @@ fn into_pathbuf(s: &str) -> Result<PathBuf, String> {
     Ok(PathBuf::from_str(s).unwrap())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let bin: Arghs = argh::from_env();
-
-    let od_args = [
-        "--disassemble",
-        " --wide",
-        "--prefix-addresses",
-        "--show-raw-insn",
-    ];
-
-    let bin_str = bin.path.as_path().to_str().expect("valid path to binary");
-
-    let stdout = Command::new("objdump")
-        .args(od_args)
-        .arg(bin_str)
-        .output()?
-        .stdout;
-
-    let stdout_utf8: String = String::from_utf8(stdout)?;
-    let mut stdout_vec: Vec<String> = Vec::new();
-    for line in stdout_utf8.lines() {
-        stdout_vec.push(line.to_string());
-    }
-
-    // capture groups: 'Address', 'Label', 'Hex', 'Instruction
-    let rgx =
-        Regex::new(r"^([0-9a-f]+)\s+<(.*)>\s+([0-9a-f][0-9a-f]( [0-9a-f][0-9a-f])*)\s+(.*?)$")?;
-
-    let extensions: Arc<Mutex<HashSet<String>>> = Arc::new(Mutex::new(HashSet::default()));
-
-    stdout_vec.into_par_iter().for_each(|line| {
-        for cap in rgx.captures_iter(&line) {
-            let hx = cap.get(3).expect("could not get third match group");
-            let ext = get_ext_zydisinfo(hx.as_str());
-            let mut guard = extensions.lock().expect("lock extension collection set");
-            let _ = guard.insert(ext);
-        }
-    });
-
-    println!("Extensions found: ");
-    let guard = extensions.lock().expect("unable to obtain lock");
-    guard.iter().for_each(|ext| print!("{ext} "));
-    println!();
-    Ok(())
+fn get_elf_data_from_path(p: &Path) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut elf_file = File::open(p)?;
+    let mut elf_buf = Vec::<u8>::new();
+    elf_file
+        .read_to_end(&mut elf_buf)
+        .expect("read file failed");
+    Ok(elf_buf)
 }
 
-fn get_ext_zydisinfo(s: &str) -> String {
-    let stdout = Command::new("ZydisInfo")
-        .arg("-64")
-        .arg(s)
-        .output()
-        .expect("zydisinfo failed")
-        .stdout;
+fn main() -> Result<(), Box<dyn Error>> {
+    let bin: Arghs = argh::from_env();
+    let bp = bin.path.as_path();
+    let elf_data = get_elf_data_from_path(bp)?;
+    let elf = Elf::from_bytes(&elf_data).expect("elf error");
 
-    let stdout_utf8: String = String::from_utf8(stdout).expect("utf8 expected");
-    let mut ext = String::new();
+    let bitness = elf.elf_header().class();
+    let bitness: u32 = match bitness {
+        ElfClass::Elf32 => 32,
+        ElfClass::Elf64 => 64,
+        ElfClass::Unknown(b) if b == 16 => 16,
+        ElfClass::Unknown(_) => return Err(String::from("unknown binary class").into()),
+    };
 
-    for line in stdout_utf8.lines() {
-        if line.contains("ISA-EXT") {
-            let (_, e) = line.split_once(':').expect("split failed");
-            let e = e.trim();
-            ext.push_str(e);
+    let text_section = elf
+        .lookup_section(b".text")
+        .expect("could not lookup .text section");
+
+    let text_section = text_section.content();
+
+    // TODO: Decoder options
+    let mut decoder = Decoder::new(bitness, text_section, DecoderOptions::NONE);
+    let mut features: HashSet<CpuidFeature> = HashSet::default();
+
+    for ins in decoder.iter() {
+        for feat in ins.cpuid_features().iter() {
+            features.insert(*feat);
         }
     }
-    ext
+
+    features.iter().for_each(|f| {
+        print!("{:?}  ", f);
+    });
+    println!();
+
+    Ok(())
 }
